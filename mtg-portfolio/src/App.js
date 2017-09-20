3fdce67f4c948d7270f6e942659e262ef3bd70c3
Mel hsieh <mehsieh89@gmail.com>
2017-09-19 18:36:05 -0700

    building simple click function

--- mtg-portfolio/src/App.js
+++ mtg-portfolio/src/App.js
@@ -3,12 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("meow");
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Melvo's Collection</h2>
+          <h2 onClick={this.handleClick}>Melvo's Collection</h2>
         </div>
       </div>
     );
